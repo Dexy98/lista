abd6e851780da29d381d80411843767c6a8dc751
Dexy98 <davidecandia@live.it>
2023-07-16 13:27:53 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import "./App.css";
+import PageTitle from "./components/PageTitle";
+
+function ToDoList() {
+  const [textItem, setTextItem] = useState("");
+  const [itemList, addItemList] = useState([]);
+
+  const onChangeItem = (e) => {
+    setTextItem(e.target.value);
+  };
+
+  const addItemToList = () => {
+    if (textItem.trim() === "") {
+      return;
+    }
+
+    addItemList([
+      ...itemList,
+      {
+        item: textItem,
+        key: Date.now(),
+        id: Math.random() * 1000,
+        completed: false,
+      },
+    ]);
+
+    localStorage.setItem("itemList", JSON.stringify(itemList));
+    setTextItem("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addItemToList();
+    }
+  };
+
+
+  const deleteItemOnList = (id) => {
+    let newList = itemList.filter((el) => el.id !== id);
+    addItemList(newList);
+  };
+
+  const CompleteItem = (id) => {
+    addItemList(
+      itemList.map((item) => {
+        if (item.id === id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div className="ToDo">
+      <div className="app mt-2">
+        <div className="header">
+          <PageTitle />
+          <input
+            className="input"
+            type="text"
+            value={textItem}
+            onChange={onChangeItem}
+            onKeyPress={handleKeyPress}
+            placeholder="Il tuo prossimo evento..."
+          />
+          <button onClick={addItemToList} className="btn">
+            Aggiungi
+          </button>
+        </div>
+        {itemList.map((el) => (
+          <ul key={el.key} className="ListItem">
+            <li className={`testoItem ${el.completed ? "completed" : ""}`}>
+              {el.item}
+            </li>
+            <div className="button">
+              <button
+                className="btnItem mr-1"
+                onClick={() => CompleteItem(el.id)}
+              >
+                ✔
+              </button>
+
+              <button
+                className="btnItem mr-1"
+                onClick={
---
+++ src/components/PageTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function PageTitle() {
+  return (
+    <div className="Titolo mb-2">
+        <h1>Lista</h1>
+    </div>
+  )
+}
+
+export default PageTitle
